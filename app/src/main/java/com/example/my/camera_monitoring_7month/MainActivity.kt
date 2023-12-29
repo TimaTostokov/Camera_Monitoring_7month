e9a46f61d85e339dab2de19b74209981f39049d1
Tolik <t1005828@gmail.com>
2023-12-29 14:54:43 +0300

    Initial commit

---
+++ app/src/main/java/com/example/my/camera_monitoring_7month/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.my.camera_monitoring_7month
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
